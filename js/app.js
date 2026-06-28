8e72233f6e1fe609259d9264dbe027debfe7b2da
Guideau <comercial151202@gmail.com>
2026-06-28 19:52:08 -0300

    add feature

--- js/app.js
+++ js/app.js
@@ -10,29 +10,77 @@ const receita = document.querySelector(`#receita`)
 const despesa = document.querySelector(`#despesas`)
 const saldo = document.querySelector(`#saldo`)
 
+const movimentacoes=[]
+ 
+function renderizarLista(){
+    lista.innerHTML = ""
+    for(const[ i, movimentacao] of movimentacoes.entries()){
+        const item = document.createElement('li')
 
+         item.innerText= (`${i} - ${movimentacao.descricao} - R$${movimentacao.valor} - ${movimentacao.tipo}`)
+        lista.appendChild(item)
+
+        }
+    }
+
+
+function atualizarResumo(){
+    let totalReceitas = 0
+    let totalDespesas = 0
+    
+
+    for(const movimentacao of movimentacoes){
+        if(movimentacao.tipo === "Saída"){
+            totalDespesas += movimentacao.valor 
+    } else {
+            totalReceitas += movimentacao.valor
+     }
+ }
+    let totalRestante= totalReceitas - totalDespesas
+    receita.innerText=`${totalReceitas}`
+    despesa.innerText=`${totalDespesas}`
+    saldo.innerText=`${totalRestante}`
+}
 
 formulario.addEventListener('submit', function(event){
     event.preventDefault()
-    const infodescricao = descricao.value
-    const infovalor = valor.value
-    const infocategoria = categoria.value
-    const infodata = data.value
+    const infodescricao = descricao.value //a descrição da entrada/saida
+    const infovalor = Number(valor.value) //valor
+    const infocategoria = categoria.value //categoria da movimentação
+    const infodata = data.value //a data
+     
+    /*Checa o Tipo da movimentação*/
+    let tipoMovimentacao
+
+    if (entrada.checked) {
+        tipoMovimentacao = "Entrada"
+    } else {
+        tipoMovimentacao = "Saída"
+    }
+
+    //guarda as movimentações
+    const movimentacao = {
+        descricao: infodescricao, 
+        valor: infovalor, 
+        categoria: infocategoria, 
+        data: infodata, 
+        tipo: tipoMovimentacao}
+    
+        movimentacoes.push(movimentacao)
    
-    
-   let tipoMovimentacao
+     if(infodescricao.trim() === ""){
+        window.alert(`Preencha o campo de Descrição`)
+     }else if(infovalor <= 0) {
+        window.alert(`Preencha o campo de Valor`)
+     }else if(infocategoria === ""){
+        window.alert(`Selecione uma Categoria valida`)
+     }else{
+   
 
-if (entrada.checked) {
-    tipoMovimentacao = "Entrada"
-} else {
-    tipoMovimentacao = "Saída"
-}
-
-console.log(tipoMovimentacao)
- 
-    console.log(infocategoria)
-    
-
-})
-
-
+            renderizarLista()
+            atualizarResumo()
+            
+        
+        }
+    }
+)
